7e36ca206a9b2f37618183a48a64414b26692765
catmanl <laurentinoluna06@protonmail.com>
2023-09-02 09:32:58 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# cuitoficio
+
+## Cargar virtual environment
+```
+$ python -m venv .
+$ ./bin/pip install -r requirements.txt
+```
+
+## Ejecutar el programa
+```
+$ ./bin/python3 main.py
+```
---
+++ main.py
@@ -0,0 +1,44 @@
+import time
+import bs4
+import requests
+import csv
+
+SECONDS_INTERVAL = 3
+
+def get_activities(cuit):
+    r = requests.get(f'https://www.cuitonline.com/detalle/{cuit}/')
+    soup = bs4.BeautifulSoup(r.text, 'html.parser')
+
+    name = soup.find('span', {'itemprop': 'name'})
+    person_data = soup.find('div', class_='persona-data')
+    activities = person_data.find('li', {'style': 'list-style: none;'}).text.split('#')
+    for elem in enumerate(activities):
+        idx = elem[0]
+        act = elem[1]
+        activities[idx] = activities[idx].strip()
+
+    return name.text, activities[1:5]
+
+f = open('cuits.csv', 'r', newline='')
+lines = f.readlines()
+data = {}
+
+for row in lines[1:]:
+    cuit = row.split(',')[2].strip()
+
+    # Remove floating point part...
+    until_float = cuit.find('.0')
+    cuit = cuit[:until_float]
+    name, acts = get_activities(cuit)
+
+    print(f'{name} listo')
+    data[name] = acts
+
+    time.sleep(SECONDS_INTERVAL)
+
+out = open('output.csv', 'w')
+writer = csv.writer(out)
+writer.writerow(['Empresa', 'Actividades'])
+for comp, acts in data.items():
+    writer.writerow([comp, acts])
+
